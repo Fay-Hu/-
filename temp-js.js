cd019cfd8f34212124a794f6d78cc4e4d3190259
Fay-Hu <479389165@qq.com>
2016-12-30 01:20:56 +0800

    Update temp-js.js

--- temp-js.js
+++ temp-js.js
@@ -26,7 +26,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		},
 		_createNS = function(tagName, attrs) {
 			var $node = $(document.createElementNS(NS_svg, tagName));
-			if (attrs) {
+			if(attrs) {
 				$node.attr(attrs);
 			}
 			return $node;
@@ -65,7 +65,9 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		},
 		DATA = {
 			fromLines: 'workflow.fromLines',
-			toLines: 'workflow.toLines'
+			toLines: 'workflow.toLines',
+			lineFrom: 'worckflow.lineFrom',
+			lineTo: 'worckflow.lineTo'
 		},
 		TEMPLATE = [
 			'<div class="workflow">',
@@ -82,7 +84,8 @@ UCD.registerWidget('Workflow', function(SUPER) {
 	return {
 		options: {
 			draggableSelector: '[data-dragtype="workflow"]',
-			dropContainerSelector: ''
+			dropContainerSelector: '',
+			arrowSize: 8
 		},
 
 		_create: function() {
@@ -93,7 +96,8 @@ UCD.registerWidget('Workflow', function(SUPER) {
 
 		_init: function() {
 			this.$branchingLine = _createNS('svg', {
-					class: [CLASSES.lineWrap, CLASSES.presetStage].join(' ')
+					class: [CLASSES.lineWrap, CLASSES.presetStage].join(' '),
+					'pointer-events': 'none'
 				})
 				.append(_createNS('path').attr({
 					class: [CLASSES.line, CLASSES.preset].join(' ')
@@ -109,7 +113,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 
 				var $this = $(e.target);
 
-				if (!$this.attr('id')) $this.attr('id', _getUID('workflow-item-'));
+				if(!$this.attr('id')) $this.attr('id', _getUID('workflow-item-'));
 
 				e = e.originalEvent;
 				e.dataTransfer.setData("Text", $this.attr('id'));
@@ -138,7 +142,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 				offset = this.$dropContainer.offset(),
 				$target = $(document.getElementById(e.originalEvent.dataTransfer.getData("Text")));
 			//防止其他结构意外拖入
-			if (!$target.is(this.options.draggableSelector)) {
+			if(!$target.is(this.options.draggableSelector)) {
 				return false;
 			}
 
@@ -170,12 +174,12 @@ UCD.registerWidget('Workflow', function(SUPER) {
 
 			this.$brachingStart = $(e.target).closest(SELECTORS.item);
 
-			if (this.$brachingStart.is(SELECTORS.itemEnd)) return false;
+			if(this.$brachingStart.is(SELECTORS.itemEnd)) return false;
 			this.branching = true;
 			this.brachingStart = [originalE.pageX - offset.left, originalE.pageY - offset.top];
 		},
 		_handleBranchMousemove: function(e) {
-			if (!this.branching) return;
+			if(!this.branching) return;
 
 			this.drawBranch = false;
 			e.stopPropagation();
@@ -190,9 +194,8 @@ UCD.registerWidget('Workflow', function(SUPER) {
 
 			this._clearPreset();
 
-			if ($target.length && !$target.is(this.$brachingStart)) {
-
-				this.$dropContainer.append(this.$presetLine = this._createLine(this.$brachingStart, $target));
+			if($target.length && !$target.is(this.$brachingStart)) {
+				this.$dropContainer.append(this.$presetLine = this._createLine(this.$brachingStart, $target).data(DATA.lineFrom, this.$brachingStart).data(DATA.lineTo, $target));
 				this.drawBranch = true;
 			} else {
 				this.$branchingLine.find('path').attr({
@@ -206,11 +209,11 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		},
 
 		_handleBranchMouseup: function(e) {
-			if (!this.branching) return;
+			if(!this.branching) return;
 
 			this.branching = false;
 
-			(this.drawBranch && this.$presetLine) && this.addLine(this.$presetLine.clone().attr('class', CLASSES.lineWrap));
+			(this.drawBranch && this.$presetLine) && this.addLine(this.$presetLine.clone(true).attr('class', CLASSES.lineWrap));
 
 			this._clearPreset();
 		},
@@ -222,12 +225,27 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		},
 
 		/**
-		 * 根据点获取连线路径
+		 * 根据起点和终点获取连线路径
 		 * @param {Object} startP
 		 * @param {Object} endP
+		 * @param {Object} isVertical
 		 */
-		_getLinePath: function(startP, endP) {
-			return ['M', startP.join(','), 'L', endP.join(',')].join(' ');
+		_getLinePath: function(startP, endP, isVertical) {
+			var
+				arrowDiffW = this.options.arrowSize,
+				arrowDiffH = arrowDiffW * 3 / 4,
+				symbolX = endP[0] - startP[0] > 0 ? 1 : -1,
+				symbolY = endP[1] - startP[1] > 0 ? 1 : -1,
+				arrowPointTop = [endP[0] - symbolX * arrowDiffW, endP[1] + arrowDiffH],
+				arrowPointBottom = [arrowPointTop[0], endP[1] - arrowDiffH],
+				arrowPointLeft = [endP[0] - arrowDiffH, endP[1] - symbolY * arrowDiffW],
+				arrowPointRight = [endP[0] + arrowDiffH, arrowPointLeft[1]];
+
+			if(isVertical) {
+				return ['M', startP.join(','), 'L', [endP[0], endP[1] - symbolY * arrowDiffW].join(','), 'L', endP.join(','), 'L', arrowPointLeft.join(','), 'M', endP.join(','), 'L', arrowPointRight.join(',')].join(' ');
+			} else {
+				return ['M', startP.join(','), 'L', [endP[0] - symbolX * arrowDiffW, endP[1]].join(','), 'L', endP.join(','), 'L', arrowPointTop.join(','), 'M', endP.join(','), 'L', arrowPointBottom.join(',')].join(' ');
+			}
 		},
 		/**
 		 * 根据起点和终点dom结构获取连线路径
@@ -244,23 +262,39 @@ UCD.registerWidget('Workflow', function(SUPER) {
 				endY = $end.position().top + scrollTop,
 				xMin = Math.min(startX, endX),
 				yMin = Math.min(startY, endY),
-				//连线方向 1表示正，-1表示负
 				deltaX = endX - startX,
 				deltaY = endY - startY,
 				//在左边的item
 				L = deltaX >= 0 ? $start : $end,
+				L_left = L.position().left + scrollLeft,
+				L_top = L.position().top + scrollTop,
 				//在右边的item
-				R = deltaX < 0 ? $start : $end;
+				R = deltaX < 0 ? $start : $end,
+				R_left = R.position().left + scrollLeft,
+				R_top = R.position().top + scrollTop,
+				T = deltaY >= 0 ? $start : $end,
+				B = deltaY < 0 ? $start : $end;
 
-			var lineStart = [L.width(), L.position().top + scrollTop - yMin + L.height() / 2],
-				lineEnd = [R.position().left + scrollLeft - xMin, R.position().top + scrollTop - yMin + R.height() / 2];
+			var arrowSize = this.options.arrowSize;
+
+			var path, lineStart, lineEnd;
+			//判断左右连线还是上下连线
+			if(R_left - L_left <= L.width()) {
+				lineStart = [L.width() / 2, L_top - yMin + (L.is(T) ? L.height() : 0)];
+				lineEnd = [R_left - xMin + R.width() / 2, R_top - yMin + (R.is(T) ? R.height() : 0)];
+				path = deltaY > 0 ? this._getLinePath(lineStart, lineEnd, true) : this._getLinePath(lineEnd, lineStart, true);
+			} else {
+				lineStart = [L.width(), L_top - yMin + L.height() / 2];
+				lineEnd = [R_left + scrollLeft - xMin, R_top - yMin + R.height() / 2];
+				path = deltaX > 0 ? this._getLinePath(lineStart, lineEnd) : this._getLinePath(lineEnd, lineStart);
+			}
 
 			return {
-				path: this._getLinePath(lineStart, lineEnd),
+				path: path,
 				//包裹线的svg的样式
 				wrapCss: {
-					width: Math.max(lineStart[0], lineEnd[0]),
-					height: Math.max(lineStart[1], lineEnd[1]),
+					width: Math.max(lineStart[0], lineEnd[0]) + arrowSize,
+					height: Math.max(lineStart[1], lineEnd[1]) + arrowSize,
 					left: xMin,
 					top: yMin
 				}
@@ -275,11 +309,13 @@ UCD.registerWidget('Workflow', function(SUPER) {
 			var linePath = this._getLinePathFromEle($start, $end);
 
 			var $line = _createNS('svg', {
-					class: CLASSES.lineWrap
+					class: CLASSES.lineWrap,
+					'pointer-events': 'none'
 				})
 				.css(linePath.wrapCss)
 				.append(_createNS('path', {
 					class: CLASSES.line,
+					'pointer-events': "visibleStroke",
 					d: linePath.path
 				}));
 
@@ -289,7 +325,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 			return $(getTplString(TEMPLATE, arguments)).clone().data(DATA.fromLines, []).data(DATA.toLines, []);
 		},
 		_clearPreset: function() {
-			this.$presetLine && this.$presetLine.remove();
+			this.$presetLine && this.deleteLine(this.$presetLine);
 			this.$branchingLine.find('path').attr({
 				d: ''
 			});
@@ -310,25 +346,31 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		 */
 		addLine: function($start, $end) {
 			var $presetLine;
-			if ($start.is(SELECTORS.lineWrap)) {
+			if($start.is(SELECTORS.lineWrap)) {
 				$presetLine = $start;
-				//$start = $presetLine.data();
-				this.$dropContainer.append($start);
+				$start = $presetLine.data(DATA.lineFrom);
+				$end = $presetLine.data(DATA.lineTo);
 			} else {
-				this.$dropContainer.append($presetLine = this._createLine($start, $end));
-				$start.data(DATA.fromLines) && $start.data(DATA.fromLines).push($presetLine);
-				$end.data(DATA.toLines) && $end.data(DATA.toLines).push($presetLine);
+				$presetLine = this._createLine($start, $end).data(DATA.lineFrom, $start).data(DATA.lineTo, $end);
 			}
+			//if($presetLine.data(DATA.lineFrom).is()) return false;
+			this.$dropContainer.append($presetLine);
+			!$start.data(DATA.fromLines) && $start.data(DATA.fromLines, []);
+			$start.data(DATA.fromLines).push($presetLine);
+			!$end.data(DATA.toLines) && $end.data(DATA.toLines, []);
+			$end.data(DATA.toLines).push($presetLine);
 		},
 		/**
 		 * 删除连线
 		 * @param {Object} $line 连线或节点，如果传入节点，删除该节点上所有连线
 		 */
 		deleteLine: function($line) {
-			if ($line.is(SELECTORS.lineWrap)) {
+			if($line.is(SELECTORS.lineWrap)) {
 				$line.remove();
-			} else if ($line.is(SELECTORS.item)) {
-				this._deleteLines();
+			} else if($line.is(SELECTORS.item)) {
+				var $item = $line;
+				this._deleteLines($item.data(DATA.fromLines));
+				this._deleteLines($item.data(DATA.toLines));
 			}
 		},
 		/**
@@ -336,9 +378,8 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		 * @param {Object} $item 可选参数，不传则重置所有线条
 		 */
 		restoreLines: function($item) {
-			if ($item) {
-				this._deleteLines($item.data(DATA.fromLines));
-				this._deleteLines($item.data(DATA.toLines));
+			if($item) {
+				this.deleteLine($item);
 			}
 		}
 	};
