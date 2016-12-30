0ad0ee4481192413f1547262bd24a9a0b1c80a7d
Fay-Hu <479389165@qq.com>
2016-12-30 17:16:46 +0800

    Update temp-js.js

--- temp-js.js
+++ temp-js.js
@@ -26,7 +26,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		},
 		_createNS = function(tagName, attrs) {
 			var $node = $(document.createElementNS(NS_svg, tagName));
-			if(attrs) {
+			if (attrs) {
 				$node.attr(attrs);
 			}
 			return $node;
@@ -59,13 +59,18 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		},
 		SELECTORS = {
 			item: '.workflow',
+			itemStart: '.start',
 			itemEnd: '.end',
+			itemSorE: '.workflow.start,.workflow.end',
 			lineWrap: '.workflow-line-wrap',
+			line: '.workflow-line',
 			dissmiss: '[data-dissmiss="workflow"]'
 		},
 		DATA = {
 			fromLines: 'workflow.fromLines',
 			toLines: 'workflow.toLines',
+			fromNodes: 'workflow.fromNodes',
+			toNodes: 'workflow.toNodes',
 			lineFrom: 'worckflow.lineFrom',
 			lineTo: 'worckflow.lineTo'
 		},
@@ -85,7 +90,11 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		options: {
 			draggableSelector: '[data-dragtype="workflow"]',
 			dropContainerSelector: '',
-			arrowSize: 8
+			arrowSize: 8,
+			onIllegalLine: $.noop,
+			enableDrop: function(e, $el, $source) {
+				return true;
+			}
 		},
 
 		_create: function() {
@@ -105,6 +114,10 @@ UCD.registerWidget('Workflow', function(SUPER) {
 				.appendTo(this.$dropContainer);
 
 			this._bindEvents();
+			this.$dropContainer.find(SELECTORS.item).draggabilly().on('dragMove', $.proxy(this._handleDragItem, this));
+
+			this.$dropContainer.find(SELECTORS.itemSorE).data(this._getInitDataKeys());
+
 			return this;
 		},
 		_bindEvents: function() {
@@ -113,7 +126,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 
 				var $this = $(e.target);
 
-				if(!$this.attr('id')) $this.attr('id', _getUID('workflow-item-'));
+				if (!$this.attr('id')) $this.attr('id', _getUID('workflow-item-'));
 
 				e = e.originalEvent;
 				e.dataTransfer.setData("Text", $this.attr('id'));
@@ -142,11 +155,15 @@ UCD.registerWidget('Workflow', function(SUPER) {
 				offset = this.$dropContainer.offset(),
 				$target = $(document.getElementById(e.originalEvent.dataTransfer.getData("Text")));
 			//防止其他结构意外拖入
-			if(!$target.is(this.options.draggableSelector)) {
+			if (!$target.is(this.options.draggableSelector)) {
 				return false;
 			}
 
 			var newItem = this._createItem($target.attr(ATTRS.icon), $target.attr(ATTRS.label) || $target.text());
+			//是否允许drop回调
+			if (!this.options.enableDrop.call(this, e, newItem, $target)) {
+				return false;
+			}
 			this.$dropContainer.append(newItem.draggabilly()
 					.on('dragMove', $.proxy(this._handleDragItem, this))
 					.css({
@@ -161,8 +178,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		_handleItemDissmiss: function(e) {
 			var $item = $(e.target).closest(SELECTORS.item);
 
-			$item.data('draggabilly') && $item.data('draggabilly').destroy();
-			$item.remove();
+			this.deleteNode($item);
 		},
 		_handleBranchMousedown: function(e) {
 			var
@@ -174,12 +190,12 @@ UCD.registerWidget('Workflow', function(SUPER) {
 
 			this.$brachingStart = $(e.target).closest(SELECTORS.item);
 
-			if(this.$brachingStart.is(SELECTORS.itemEnd)) return false;
+			if (this.$brachingStart.is(SELECTORS.itemEnd)) return false;
 			this.branching = true;
 			this.brachingStart = [originalE.pageX - offset.left, originalE.pageY - offset.top];
 		},
 		_handleBranchMousemove: function(e) {
-			if(!this.branching) return;
+			if (!this.branching) return;
 
 			this.drawBranch = false;
 			e.stopPropagation();
@@ -194,7 +210,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 
 			this._clearPreset();
 
-			if($target.length && !$target.is(this.$brachingStart)) {
+			if ($target.length && !$target.is(this.$brachingStart)) {
 				this.$dropContainer.append(this.$presetLine = this._createLine(this.$brachingStart, $target).data(DATA.lineFrom, this.$brachingStart).data(DATA.lineTo, $target));
 				this.drawBranch = true;
 			} else {
@@ -209,7 +225,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		},
 
 		_handleBranchMouseup: function(e) {
-			if(!this.branching) return;
+			if (!this.branching) return;
 
 			this.branching = false;
 
@@ -221,7 +237,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 			$(e.target).closest('svg').remove();
 		},
 		_handleDragItem: function(e, pointer, moveVector) {
-			this.restoreLines($(e.currentTarget));
+			this.resetLines.call(this, $(e.currentTarget));
 		},
 
 		/**
@@ -233,7 +249,8 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		_getLinePath: function(startP, endP, isVertical) {
 			var
 				arrowDiffW = this.options.arrowSize,
-				arrowDiffH = arrowDiffW * 3 / 4,
+				arrowDiffH = Math.round(arrowDiffW * 3 / 4),
+				arrowDiff = Math.round(arrowDiffW / 3 * 4),
 				symbolX = endP[0] - startP[0] > 0 ? 1 : -1,
 				symbolY = endP[1] - startP[1] > 0 ? 1 : -1,
 				arrowPointTop = [endP[0] - symbolX * arrowDiffW, endP[1] + arrowDiffH],
@@ -241,10 +258,16 @@ UCD.registerWidget('Workflow', function(SUPER) {
 				arrowPointLeft = [endP[0] - arrowDiffH, endP[1] - symbolY * arrowDiffW],
 				arrowPointRight = [endP[0] + arrowDiffH, arrowPointLeft[1]];
 
-			if(isVertical) {
-				return ['M', startP.join(','), 'L', [endP[0], endP[1] - symbolY * arrowDiffW].join(','), 'L', endP.join(','), 'L', arrowPointLeft.join(','), 'M', endP.join(','), 'L', arrowPointRight.join(',')].join(' ');
+			if (isVertical) {
+				return [
+					'M', startP.join(','), 'L', [endP[0], endP[1] - symbolY * arrowDiff].join(','), 'L', [endP[0], endP[1] - symbolY * arrowDiffW].join(','),
+					'L', arrowPointLeft.join(','), 'L', endP.join(','), 'L', arrowPointRight.join(','), 'L', [endP[0], endP[1] - symbolY * arrowDiffW].join(','), 'L', [endP[0], endP[1] - symbolY * arrowDiff].join(','), 'Z'
+				].join(' ');
 			} else {
-				return ['M', startP.join(','), 'L', [endP[0] - symbolX * arrowDiffW, endP[1]].join(','), 'L', endP.join(','), 'L', arrowPointTop.join(','), 'M', endP.join(','), 'L', arrowPointBottom.join(',')].join(' ');
+				return [
+					'M', startP.join(','), 'L', [endP[0] - symbolX * arrowDiff, endP[1]].join(','), 'L', [endP[0] - symbolX * arrowDiffW, endP[1]].join(','),
+					'L', arrowPointTop.join(','), 'L', endP.join(','), 'L', arrowPointBottom.join(','), 'L', [endP[0] - symbolX * arrowDiffW, endP[1]].join(','), 'L', [endP[0] - symbolX * arrowDiff, endP[1]].join(','), 'Z'
+				].join(' ');
 			}
 		},
 		/**
@@ -279,7 +302,7 @@ UCD.registerWidget('Workflow', function(SUPER) {
 
 			var path, lineStart, lineEnd;
 			//判断左右连线还是上下连线
-			if(R_left - L_left <= L.width()) {
+			if (R_left - L_left <= L.width()) {
 				lineStart = [L.width() / 2, L_top - yMin + (L.is(T) ? L.height() : 0)];
 				lineEnd = [R_left - xMin + R.width() / 2, R_top - yMin + (R.is(T) ? R.height() : 0)];
 				path = deltaY > 0 ? this._getLinePath(lineStart, lineEnd, true) : this._getLinePath(lineEnd, lineStart, true);
@@ -321,8 +344,19 @@ UCD.registerWidget('Workflow', function(SUPER) {
 
 			return $line;
 		},
+		_getInitDataKeys: function() {
+			var keys = {};
+
+			keys[DATA.fromLines] = [];
+			keys[DATA.toLines] = [];
+			keys[DATA.fromNodes] = [];
+			keys[DATA.toNodes] = [];
+
+			return keys;
+		},
 		_createItem: function() {
-			return $(getTplString(TEMPLATE, arguments)).clone().data(DATA.fromLines, []).data(DATA.toLines, []);
+
+			return $(getTplString(TEMPLATE, arguments)).clone().data(this._getInitDataKeys());
 		},
 		_clearPreset: function() {
 			this.$presetLine && this.deleteLine(this.$presetLine);
@@ -335,8 +369,11 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		 * @param {Array} lines
 		 */
 		_deleteLines: function(lines) {
-			$.each(lines, function(v, i) {
-				$(v).off().remove();
+			if (!$.isArray(lines)) return;
+
+			var self = this;
+			$.each(lines, function(i, v) {
+				self.deleteLine($(v));
 			});
 		},
 		/**
@@ -346,40 +383,56 @@ UCD.registerWidget('Workflow', function(SUPER) {
 		 */
 		addLine: function($start, $end) {
 			var $presetLine;
-			if($start.is(SELECTORS.lineWrap)) {
+			if ($start.is(SELECTORS.lineWrap)) {
 				$presetLine = $start;
 				$start = $presetLine.data(DATA.lineFrom);
 				$end = $presetLine.data(DATA.lineTo);
 			} else {
 				$presetLine = this._createLine($start, $end).data(DATA.lineFrom, $start).data(DATA.lineTo, $end);
 			}
-			//if($presetLine.data(DATA.lineFrom).is()) return false;
+
+			//TODO inArray
+			//if (~$.inArray($end, $start.data(DATA.fromNodes)) || ~$.inArray($end, $start.data(DATA.toNodes))) return this.options.onIllegalLine.call(this, $start, $end);
+
 			this.$dropContainer.append($presetLine);
-			!$start.data(DATA.fromLines) && $start.data(DATA.fromLines, []);
 			$start.data(DATA.fromLines).push($presetLine);
-			!$end.data(DATA.toLines) && $end.data(DATA.toLines, []);
+			$start.data(DATA.toNodes).push($end);
 			$end.data(DATA.toLines).push($presetLine);
+			$end.data(DATA.fromNodes).push($start);
 		},
 		/**
 		 * 删除连线
-		 * @param {Object} $line 连线或节点，如果传入节点，删除该节点上所有连线
+		 * @param {Object} $line
 		 */
 		deleteLine: function($line) {
-			if($line.is(SELECTORS.lineWrap)) {
-				$line.remove();
-			} else if($line.is(SELECTORS.item)) {
-				var $item = $line;
-				this._deleteLines($item.data(DATA.fromLines));
-				this._deleteLines($item.data(DATA.toLines));
-			}
+			($line instanceof $) && $line.off().remove();
+		},
+		/**
+		 * 删除节点
+		 * @param {Object} $node
+		 */
+		deleteNode: function($node) {
+			this._deleteLines($node.data(DATA.fromLines));
+			this._deleteLines($node.data(DATA.toLines));
+			$node.data('draggabilly') && $node.data('draggabilly').destroy();
+			$node.remove();
 		},
 		/**
 		 * 重置线条
-		 * @param {Object} $item 可选参数，不传则重置所有线条
+		 * @param {Object} $node 节点
 		 */
-		restoreLines: function($item) {
-			if($item) {
-				this.deleteLine($item);
+		resetLines: function($node) {
+			var self = this;
+
+			if ($node) {
+				//遍历节点相关连线 ,重新绘制
+				$.each($node.data(DATA.fromLines).concat($node.data(DATA.toLines)), function(i, v) {
+					var
+						$line = $(v),
+						linePath = self._getLinePathFromEle($line.data(DATA.lineFrom), $line.data(DATA.lineTo));
+
+					$line.css(linePath.wrapCss).find(SELECTORS.line).attr('d', linePath.path);
+				});
 			}
 		}
 	};
