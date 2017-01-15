21892d443ac303b764a15174b946fc38fa0256b3
Fay-Hu <479389165@qq.com>
2017-01-16 01:14:00 +0800

    Update temp-js.js

--- temp-js.js
+++ temp-js.js
@@ -1,44 +1,42 @@
 const component = {
   template: model => `
 	  <div class="searcher">
-	      <input type="text" value="${model.selected.value}" placeholder="please input"/>
+	      <input type="text" value="${model.filter}" placeholder="${model.placeHolder}"/>
 	      <ul class="list">
-	      ${model.items.map(item => `
-	        <li class="item">${item.value}</li>
-        `).join('')}
+	  ${model.items.map(item => ~item.text.search(model.filter) ? `
+           <li class="item">${item.text}</li>`
+    : '').join('')}
 	      </ul>
 	  </div>
 	`,
   style: `
-	  .list{
-	    background:black;
-	    padding:0;
-	  }
-	  .item{
-	    margin:0;
-	    padding:.5em;
-	    background:#36bd9c;
-	    list-style:none;
-	    color:white;
-	  }
-	  .item:hover{
-	    background:#239bc8;
-	  }
-	  input{
-	    height: 30px;
-      padding: .3em;
-	  }
-	  input:focus{
-	    border-color:transparent;
-	    outline:none;
-	    box-shadow:0 0 1px 2px #239bc8;
-	  }
+    <style>
+      .list{
+        padding:0;
+        background:black;
+      }
+      .item{
+        margin:0;
+        padding:.5em;
+        color:white;
+        background:#36bd9c;
+        list-style:none;   
+      }
+      .item:hover{
+        background:#239bc8;
+      }
+      input{
+        height: 30px;
+        padding: .3em;
+      }
+      input:focus{
+        border-color:transparent;
+        outline:none;
+        box-shadow:0 0 1px 2px #239bc8;
+      }
+	  </style>
 	`,
   selector: 'x-searcher'
-};
-
-const inputs = {
-  model: 'data-model'
 };
 
 class Searcher extends HTMLElement {
@@ -46,36 +44,55 @@ class Searcher extends HTMLElement {
     super();
 
     this.$shadowRoot = this.attachShadow({mode: 'closed'});
-    this.model = this.getAttribute(inputs.model) || {
-        selected: {
-          value: 'red'
-        },
-        items: [{
-          value: 'blue'
-        }, {
-          value: 'yellow'
-        }]
-      };
-    this.render()._observer();
+    this.model = {
+      filter: '',
+      placeHolder: 'please input keywords',
+      items: [{
+        value: '01',
+        text: 'blue'
+      }, {
+        value: '02',
+        text: 'yellow'
+      }, {
+        value: '03',
+        text: 'red'
+      }]
+    };
+    this.render();
   }
 
   render() {
-    this.$shadowRoot.innerHTML = '<style>' + component.style + '</style>' + component.template(this.model);
-    return this;
+    this.$shadowRoot.innerHTML = component.style + component.template(this.model);
+    this.$input = this.$shadowRoot.querySelector('input');
+    this.$input.focus();
+
+    return this._observer()._proxy();
   }
 
   _observer() {
-    new MutationObserver(this._filter.bind(this))
-      .observe(this.$shadowRoot.querySelector('input'),{
-        characterData: true,
+    this.$input.addEventListener('input', (e) => {
+      e.target.setAttribute('value', this.model.filter = e.target.value);
+    });
+
+    new MutationObserver(this.render.bind(this))
+      .observe(this.$input, {
         attributes: true
       });
+
+    return this;
   }
 
-  _filter(){
-    console.log(arguments)
+  _proxy() {
+    var _this = this;
+    this.model = new Proxy(this.model, {
+      set(target, key, value, proxy){
+        _this.render.call(_this);
+        return Reflect.set(target, key, value, proxy);
+      }
+    });
+
+    return this;
   }
 }
 
 customElements.define(component.selector, Searcher);
-
