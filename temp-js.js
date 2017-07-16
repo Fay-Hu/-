491ae496be38abfbd54258c62ddf1edca2bed175
Fay-Hu <479389165@qq.com>
2017-07-17 02:13:58 +0800

    Update temp-js.js

--- temp-js.js
+++ temp-js.js
@@ -1,109 +1,135 @@
-/**
- * Created by hwx336970 on 2017.01.18
- *
- * @memberOf UCD
- * @class Sliding
- * @augments UCD.Widget
- */
-UCD.registerWidget('Sliding', function (SUPER) {
-  var $ = UCD.require('jquery');
-  return {
-    options: {
-      autoPlay: false,
-      loop: false,
-      initIndex: 0,
-      duration: 500,
-      interval: 3000,
-      countPadding: true,
-      onChanged: $.noop,
-      //should with style "white-space:nowrap;overflow:hidden"
-      slideSelector: '.sliding-content',
-      prevSelector: '.prev',
-      nextSelector: '.next'
-    },
 
-    _create: function () {
-      SUPER._create.call(this);
-      this.timer = null;
-      this.current = 0;
-    },
+'use strict'
 
-    _init: function () {
-      var
-        opts = this.options,
-        ele = this.element;
+const easeInOut = function (t, b, c, d) {
+    if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+    return -c / 2 * ((--t) * (t - 2) - 1) + b;
+}
+class Particles {
+    constructor(ele, opts) {
+        ele instanceof HTMLCanvasElement ? this.canvas = ele : ele.appendChild(this.canvas = document.createElement('canvas'))
 
-      this.$box = opts.slideSelector !== '' ? ele.find(opts.slideSelector) : this.element;
-      this._on(this.$prev = ele.find(opts.prevSelector), {
-        'click': 'prev'
-      });
-      this._on(this.$next = ele.find(opts.nextSelector), {
-        'click': 'next'
-      });
+        this.ctx = this.canvas.getContext('2d')
+        this._options = Object.assign({}, Particles.defaults, opts)
 
-      this._on(this.window, {'resize': 'go'});
+        this.particles = [];
 
-      this.element.on('click', [opts.prevSelector, opts.nextSelector].join(','), $.proxy(this.pause, this));
-      this.go(opts.initIndex);
-      if (opts.autoPlay) this.play();
-    },
+        this._initCanvas()
+        window.addEventListener('resize', this._initCanvas.bind(this))
 
-    getCurrent: function () {
-      //in order to respond to the window,recount the cuttent index every time
-      return this.current = this.$box.scrollLeft() === 0 ? 0 : Math.ceil(this.$box.scrollLeft() / this.$box.width());
-    },
-
-    prev: function () {
-      this.getCurrent();
-      this.go(--this.current);
-      return this;
-    },
-
-    next: function () {
-      this.getCurrent();
-      this.go(++this.current);
-      return this;
-    },
-
-    go: function (index) {
-      var
-        _this = this,
-        $box = this.$box,
-        loop = this.options.loop,
-        childen = this.$box.children(),
-
-        //need the items equal width
-        max_i = Math.ceil(childen.length * childen.outerWidth(true) / $box.outerWidth()) - 1;
-
-      if (typeof index !== 'number') index = this.current;
-      this.current = index = index < 0 ? (loop ? max_i : 0) : index > max_i ? (loop ? 0 : max_i) : index;
-
-      if (!loop) {
-
-        this.$prev.toggleClass('disabled', this.current === 0);
-        this.$next.toggleClass('disabled', this.current === max_i);
-      }
-
-      this.$box.stop().animate({
-        'scrollLeft': $box.width() * index + (this.options.countPadding && index !== 0 ? $box.children().css('padding-left').replace(/px/, '') * 2 : 0)
-      }, this.options.duration, 'swing', function () {
-        //call chenged
-        _this.options.onChanged.call(_this, index);
-        if (_this.options.autoPlay && !_this.timer) _this.play();
-      });
-
-      return this;
-    },
-
-    play: function () {
-      this.timer = window.setInterval($.proxy(this.next, this), this.options.interval);
-      return this;
-    },
-
-    pause: function () {
-      window.clearInterval(this.timer);
-      this.timer = null;
-      return this;
+        this.init()
     }
-  };
-});
+
+    static get defaults() {
+        return {
+            imgUrl: 'assets/images/test2.jpg',
+            density: 4,
+            duration: 100
+        }
+    }
+
+    get options() {
+        return this._options
+    }
+
+    set options(opts) {
+        Object.assign(this._options, opts)
+    }
+
+    init() {
+        let opts = this.options
+
+        this._loadImage(this.options.imgUrl, (img) => {
+            this.ctx.drawImage(img, 0, 0, img.width, img.height)
+            let imgData = this.ctx.getImageData(0, 0, this.canvas.clientWidth, this.canvas.clientHeight)
+
+            let
+                density = opts.density,
+                particleCols = img.width / density,
+                particleRows = img.height / density
+
+            for (let i = 0; i < particleRows; i++)
+                for (let j = 0; j < particleCols; j++) {
+                    var rgba = this._getRgba(imgData, i * density, j * density)
+                    if (rgba[0] < 250) {
+                        this.particles.push({
+                            x: Math.random() * this.canvas.clientWidth,
+                            y: Math.random() * this.canvas.clientHeight,
+                            tX: img.x + i * density + Math.random() * density,
+                            tY: img.y + j * density + Math.random() * density,
+                            style: `rgba(${rgba.join(',')})`
+                        })
+                    }
+                }
+
+            this.animate()
+        })
+    }
+
+    render() {
+        let ctx = this.ctx
+
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.particles.forEach((v) => {
+            ctx.fillStyle = v.style
+            ctx.fillRect(v.x, v.y, 1, 1)
+        })
+    }
+
+    animate() {
+        let start = 0,
+            during = this.options.duration
+
+        let _run = () => {
+            start++
+            this.particles.forEach((v, i) => {
+                v.x = easeInOut(start, v.x, v.tX - v.x, during)
+                v.y = easeInOut(start, v.y, v.tY - v.y, during)
+            })
+            this.render()
+
+            if (start < during)
+                requestAnimationFrame(_run)
+        }
+        requestAnimationFrame(_run)
+    }
+
+    _initCanvas() {
+        var
+            devicePixelRatio = devicePixelRatio || 1, //dpr
+            width = devicePixelRatio * this.canvas.clientWidth,
+            height = devicePixelRatio * this.canvas.clientHeight
+
+        this.canvas.setAttribute('width', width)
+        this.canvas.setAttribute('height', height)
+
+        this.canvas.style.width = width / devicePixelRatio + 'px'
+        this.canvas.style.height = height / devicePixelRatio + 'px'
+    }
+
+    _loadImage(url, callback) {
+        var img = new Image()
+        img.src = url
+
+        // cache 
+        if (img.complete) {
+            callback(img)
+            return img
+        }
+
+        img.onload = () => {
+            callback(img)
+        }
+
+        return img
+    }
+
+    _getRgba(imageData, i, j) {
+        return [
+            imageData.data[((j - 1) * imageData.width + (i - 1)) * 4],
+            imageData.data[((j - 1) * imageData.width + (i - 1)) * 4 + 1],
+            imageData.data[((j - 1) * imageData.width + (i - 1)) * 4 + 2],
+            imageData.data[((j - 1) * imageData.width + (i - 1)) * 4 + 3]
+        ]
+    }
+}
